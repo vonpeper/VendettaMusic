39c6d24653d0f50b5f83af7f65c7c40e9980a524
vonpeper <josebautista2104@gmail.com>
2026-08-05 13:46:05 -0600

    fix: explicitly define secret in authConfig to fix JWTSessionError

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -2,6 +2,7 @@ import type { NextAuthConfig } from "next-auth"
 
 export const authConfig = {
   providers: [],
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
