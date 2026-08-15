3193fd8ca504477b93c25415310fabd7da82ba9a
Pepe Bautista <vonpeper@MacBook-Air-de-Pepe.local>
2026-08-15 13:01:03 -0600

    chore(debug): use curl -k -L

--- scripts/get_vps_logs.py
+++ scripts/get_vps_logs.py
@@ -53,7 +53,7 @@ def fetch_logs():
         stdin, stdout, stderr = ssh.exec_command("docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' vendetta-prod-gcqoaf-vendetta-app-1")
         container_ip = stdout.read().decode('utf-8').strip()
         
-        stdin, stdout, stderr = ssh.exec_command(f'curl -v -H "Host: vendetta.mx" "http://127.0.0.1/api/admin/contract/349d67ae-ee3f-44c5-a0d8-9a37ffa77b65?token={expected_token}"')
+        stdin, stdout, stderr = ssh.exec_command(f'curl -v -k -L -H "Host: vendetta.mx" "http://127.0.0.1/api/admin/contract/349d67ae-ee3f-44c5-a0d8-9a37ffa77b65?token={expected_token}"')
         curl_output = stdout.read().decode('utf-8')
         curl_err = stderr.read().decode('utf-8')
             
